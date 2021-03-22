ece86b416ce00f4da83225b652ed30bf701d121a
mbell0 <80069352+mbell0@users.noreply.github.com>
2021-03-22 22:13:09 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
